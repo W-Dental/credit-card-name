e771d5af7448be63bd9c9b66b116b30c7ebea14d
Marcos Martins <marcos.martins@woozaventures.com.br>
2020-05-15 12:48:30 -0300

    imporv: refactoring funcionalitty

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { CREDIT_CARD_CONFIG } from './constant';
 import { CreditCardConfig } from '../types/CreditCardConfig';
 
-export default function getCreditCardOperatorByInitalsNumber(creditNumber: string = ''): string {
+export default function getCreditCardOperatorByInitalsNumber(creditNumber = ''): string {
   try {
     const normalizedCreditNumber = creditNumber.replace(/\s/g, '');
     const result =  CREDIT_CARD_CONFIG.find(creditcard => creditcard.regexpBin.test(normalizedCreditNumber));
-      return (result as CreditCardConfig).name;
+    return result!.name;
   } catch(e) {
-    throw `${creditNumber} is an invalid value, please enter with a valid credit card number!`
+    throw `${creditNumber} is an invalid value or type, please see documentations for more infos!`
   }
 }
