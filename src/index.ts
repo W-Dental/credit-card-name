1f0b66fc04a20a03ec4ad6b6666e99936b82f330
Marcos Martins <marcos.martins@woozaventures.com.br>
2020-05-15 12:01:24 -0300

    fiz: remove unecessary cast

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { CreditCardConfig } from '../types/CreditCardConfig';
 export default function getCreditCardOperatorByInitalsNumber(creditNumber: string = ''): string {
   try {
     const normalizedCreditNumber = creditNumber.replace(/\s/g, '');
-    const result =  CREDIT_CARD_CONFIG.find(creditcard => !!creditcard.regexpBin.test(normalizedCreditNumber));
+    const result =  CREDIT_CARD_CONFIG.find(creditcard => creditcard.regexpBin.test(normalizedCreditNumber));
       return (result as CreditCardConfig).name;
   } catch(e) {
     throw `${creditNumber} is an invalid value, please enter with a valid credit card number!`
